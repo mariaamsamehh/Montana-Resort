be1bd2ad4c4d55fa6f81b79d4e0e956dd052770d
mariaamsamehh <134538955+mariaamsamehh@users.noreply.github.com>
2023-12-03 00:07:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,488 @@
+// script.js login
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Find the "Login" link
+    var loginLink = document.querySelector('a[href="#login"]');
+  
+    // Add a click event listener
+    loginLink.addEventListener('click', function(event) {
+      // Prevent the default behavior of the link
+      event.preventDefault();
+  
+      // Redirect the user to the login page
+      window.location.href = 'login.html'; // Update with the correct path to your login page
+    });
+  });
+
+  // script.js sign up
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Find the "Sign Up" link
+    var signUpLink = document.querySelector('a[href="#signup"]');
+  
+    // Add a click event listener
+    signUpLink.addEventListener('click', function(event) {
+      // Prevent the default behavior of the link
+      event.preventDefault();
+  
+      // Redirect the user to the sign-up page
+      window.location.href = 'signup.html'; // Update with the correct path to your sign-up page
+    });
+  });
+
+
+  // script.js sign up
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Find the "Sign Up" link
+    var signUpLink = document.querySelector('a[href="#overview"]');
+  
+    // Add a click event listener
+    signUpLink.addEventListener('click', function(event) {
+      // Prevent the default behavior of the link
+      event.preventDefault();
+  
+      // Redirect the user to the sign-up page
+      window.location.href = 'overview.html'; // Update with the correct path to your sign-up page
+    });
+  });
+  
+  // script.js sign up
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Find the "Sign Up" link
+    var signUpLink = document.querySelector('a[href="#rooms"]');
+  
+    // Add a click event listener
+    signUpLink.addEventListener('click', function(event) {
+      // Prevent the default behavior of the link
+      event.preventDefault();
+  
+      // Redirect the user to the sign-up page
+      window.location.href = 'rooms.html'; // Update with the correct path to your sign-up page
+    });
+  });
+
+  // script.js sign up
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Find the "Sign Up" link
+    var signUpLink = document.querySelector('a[href="#weddings"]');
+  
+    // Add a click event listener
+    signUpLink.addEventListener('click', function(event) {
+      // Prevent the default behavior of the link
+      event.preventDefault();
+  
+      // Redirect the user to the sign-up page
+      window.location.href = 'weddings.html'; // Update with the correct path to your sign-up page
+    });
+  });
+  // script.js sign up
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Find the "Sign Up" link
+    var signUpLink = document.querySelector('a[href="#contact"]');
+  
+    // Add a click event listener
+    signUpLink.addEventListener('click', function(event) {
+      // Prevent the default behavior of the link
+      event.preventDefault();
+  
+      // Redirect the user to the sign-up page
+      window.location.href = 'contact.html'; // Update with the correct path to your sign-up page
+    });
+  });
+  
+  // script.js sign up
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Find the "Sign Up" link
+    var signUpLink = document.querySelector('a[href="#spa"]');
+  
+    // Add a click event listener
+    signUpLink.addEventListener('click', function(event) {
+      // Prevent the default behavior of the link
+      event.preventDefault();
+  
+      // Redirect the user to the sign-up page
+      window.location.href = 'spa.html'; // Update with the correct path to your sign-up page
+    });
+  });
+
+  // script.js sign up
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Find the "Sign Up" link
+    var signUpLink = document.querySelector('a[href="#reservation"]');
+  
+    // Add a click event listener
+    signUpLink.addEventListener('click', function(event) {
+      // Prevent the default behavior of the link
+      event.preventDefault();
+  
+      // Redirect the user to the sign-up page
+      window.location.href = 'reservation.html'; // Update with the correct path to your sign-up page
+    });
+  });
+  
+  // script.js sign up
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Find the "Sign Up" link
+    var signUpLink = document.querySelector('a[href="#gym"]');
+  
+    // Add a click event listener
+    signUpLink.addEventListener('click', function(event) {
+      // Prevent the default behavior of the link
+      event.preventDefault();
+  
+      // Redirect the user to the sign-up page
+      window.location.href = 'gym.html'; // Update with the correct path to your sign-up page
+    });
+  });
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Find the "Sign Up" link
+    var signUpLink = document.querySelector('a[href="#dining"]');
+  
+    // Add a click event listener
+    signUpLink.addEventListener('click', function(event) {
+      // Prevent the default behavior of the link
+      event.preventDefault();
+  
+      // Redirect the user to the sign-up page
+      window.location.href = 'dining.html'; // Update with the correct path to your sign-up page
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // Find the "Sign Up" link
+    var signUpLink = document.querySelector('a[href="#restaurant1"]');
+  
+    // Add a click event listener
+    signUpLink.addEventListener('click', function(event) {
+      // Prevent the default behavior of the link
+      event.preventDefault();
+  
+      // Redirect the user to the sign-up page
+      window.location.href = 'restaurant1.html'; // Update with the correct path to your sign-up page
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // Find the "Sign Up" link
+    var signUpLink = document.querySelector('a[href="#restaurant2"]');
+  
+    // Add a click event listener
+    signUpLink.addEventListener('click', function(event) {
+      // Prevent the default behavior of the link
+      event.preventDefault();
+  
+      // Redirect the user to the sign-up page
+      window.location.href = 'restaurant2.html'; // Update with the correct path to your sign-up page
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // Find the "Sign Up" link
+    var signUpLink = document.querySelector('a[href="#restaurant3"]');
+  
+    // Add a click event listener
+    signUpLink.addEventListener('click', function(event) {
+      // Prevent the default behavior of the link
+      event.preventDefault();
+  
+      // Redirect the user to the sign-up page
+      window.location.href = 'restaurant3.html'; // Update with the correct path to your sign-up page
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // Find the "Sign Up" link
+    var signUpLink = document.querySelector('a[href="#transportation"]');
+  
+    // Add a click event listener
+    signUpLink.addEventListener('click', function(event) {
+      // Prevent the default behavior of the link
+      event.preventDefault();
+  
+      // Redirect the user to the sign-up page
+      window.location.href = 'transportation.html'; // Update with the correct path to your sign-up page
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // Find the "Sign Up" link
+    var signUpLink = document.querySelector('a[href="#attractions"]');
+  
+    // Add a click event listener
+    signUpLink.addEventListener('click', function(event) {
+      // Prevent the default behavior of the link
+      event.preventDefault();
+  
+      // Redirect the user to the sign-up page
+      window.location.href = 'attractions.html'; // Update with the correct path to your sign-up page
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // Find the "Sign Up" link
+    var signUpLink = document.querySelector('a[href="#shopping"]');
+  
+    // Add a click event listener
+    signUpLink.addEventListener('click', function(event) {
+      // Prevent the default behavior of the link
+      event.preventDefault();
+  
+      // Redirect the user to the sign-up page
+      window.location.href = 'shopping.html'; // Update with the correct path to your sign-up page
+    });
+  });
+
+// scroll images in trasnp >>>>>>>>>>>>>>>>>>>
+
+  let currentIndex = 0;
+
+  function scrollTransportation(direction) {
+    const container = document.querySelector('.transportation-section');
+    const content = document.querySelectorAll('.transportation-content');
+    const contentWidth = content[0].clientWidth;
+
+    currentIndex = (currentIndex + direction) % content.length;
+    const translateX = -currentIndex * contentWidth;
+    container.style.transform = `translateX(${translateX}px)`;
+  }
+
+  
+// signup page >>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+  function submitForm() {
+    // Prevent the default form submission
+    event.preventDefault();
+  
+    // Get form data
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const gender = document.getElementById('gender').value;
+  
+    // Validate the form data
+    if (username.trim() === '' || email.trim() === '' || password.trim() === '') {
+      alert('Please fill in all required fields.');
+      return;
+    }
+   
+  
+    // Additional validation logic (e.g., email format, password strength) can be added here
+  
+    // Create an object with user data
+    const userData = {
+      username: username,
+      email: email,
+      password: password,
+      gender: gender
+    };
+  
+    // Log the user data to the console (for demonstration purposes)
+    console.log('User Data:', userData);
+  
+    // You can add additional logic here, such as sending data to a server or redirecting the user
+    window.location.href = 'login.html';
+   
+  }
+
+  
+
+//   loginn page>>>>>>>>>>>>>>>>>>
+document.addEventListener('DOMContentLoaded', function () {
+    const loginForm = document.getElementById('loginform');
+
+    const users = [
+        { email: 'mario.sameh', password: '123mariam' },
+        { email: '20p1599.sis', password: '1511mariam' },
+       
+    ];
+
+    loginForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const enteredEmail = document.getElementById('login-username').value;
+        const enteredPassword = document.getElementById('login-password').value;
+
+        // Check if the entered credentials match any user in the array
+        const matchedUser = users.find(user => user.email === enteredEmail && user.password === enteredPassword);
+
+        if (matchedUser) {
+            alert('Login successful!');
+            window.location.href = 'reservation.html'; 
+        } else {
+            alert('Invalid email or password'); 
+         }    
+     });
+});
+
+function submitReservation(event) {
+    // Prevent the default form submission
+    event.preventDefault();
+
+    // Get form data
+    const checkInDate = document.getElementById('check-in').value;
+    const checkOutDate = document.getElementById('check-out').value;
+    const roomType = document.getElementById('room-type').value;
+    const includeBreakfast = document.getElementById('breakfast').checked;
+
+    // Create an object with reservation data
+    const reservationData = {
+      checkInDate: checkInDate,
+      checkOutDate: checkOutDate,
+      roomType: roomType,
+      includeBreakfast: includeBreakfast,
+    };
+
+    // Log the reservation data to the console (for demonstration purposes)
+    console.log('Reservation Data:', reservationData);
+    window.location.href = 'checkout.html';
+    
+  }
+
+// overview page>>>>>>>>>>>>>>>>>>>
+  document.addEventListener('DOMContentLoaded', function () {
+    // Add a fade-in effect to the content
+    const content = document.querySelector('.content');
+    content.style.opacity = '0';
+    fadeIn(content, 0);
+
+    function fadeIn(element, opacity) {
+      let currentOpacity = opacity;
+      const intervalId = setInterval(function () {
+        if (currentOpacity < 1) {
+          currentOpacity += 0.1;
+          element.style.opacity = currentOpacity;
+        } else {
+          clearInterval(intervalId);
+        }
+      }, 100);
+    }
+  });
+
+//   wedding page>>>>>>>>>>>>>>>>>>>
+
+const video = document.querySelector('.video');
+
+    // Toggle play/pause on video click
+    video.addEventListener('click', function () {
+      if (video.paused) {
+        video.play();
+      } else {
+        video.pause();
+      }
+    });
+
+// shopping page>>>>>>>>>>>>>>>>>>>
+    
+function redirectToSite(place) {
+    // Define URLs for each shop
+    const shopUrls = {
+      'ubud': 'https://www.ubudcenter.com/ubud-art-market/',
+      'beachwalk': 'https://www.beachwalkbali.com/',
+      'seminyak': 'https://nyamangallery.com/'
+    };
+
+    // Get the URL for the clicked place
+    const url = shopUrls[place];
+
+    // Open the link in a new tab
+    window.open(url, '_blank');
+  }
+
+// change button color in gym class style>>>>>>>>>>>>>>>>>>
+  function changeStyle() {
+    const changeContent = document.getElementById('changeContent');
+    changeContent.style.color = 'black';
+    //changeContent.style.fontWeight = 'bold';
+    changeContent.style.backgroundColor = 'grey';
+    window.open('assg 1.html', '_blank');
+}
+
+// change photo in overview page and gym page >>>>>>>>>>>>>>>>>
+function changeImage() {
+    const imageElement = document.querySelector('.hotel-image');
+    
+    if (imageElement.src.includes('overview_photo.png')) {
+        // Change to the second image
+        imageElement.src = 'change.png';
+    } else {
+        // Change back to the first image
+        imageElement.src = 'overview_photo.png';
+    }
+}
+
+function changeImage() {
+    const imageElement = document.querySelector('.gy-image');
+    
+    if (imageElement.src.includes('gym_img.jpg')) {
+        // Change to the second image
+        imageElement.src = 'gym2.webp';
+    } else {
+        // Change back to the first image
+        imageElement.src = 'gym_img.jpg';
+    }
+}
+
+
+
+// hide and show elements in rooms page>>>>>>>>>>>>>>>>>
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+//   pop up login in transportation page>>>>>>>>>>>>>>>>>
+ function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+  
+// open prompt in spa/gym page for review>>>>>>>>>>>>>>>>>>>>
+
+  function openEditPrompt() {
+    // Use prompt to get user input
+    const userInput = prompt("Enter new text for the spa paragraph:");
+
+    // Check if the user clicked "OK" and entered a value
+    if (userInput !== null) {
+        // Update the paragraph's content with the user input
+        document.getElementById('spaText').innerText = userInput;
+    } else {
+        // Handle the case where the user clicked "Cancel"
+        alert("Edit canceled");
+    }
+}
+
+
+// checkout page>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+function showSuccessMessage() {
+    alert("Room reserved successfully! Thank you for choosing our hotel");
+    
+
+}
+
+// hide and show an iframe video in attractions page>>>>>>>>>>>>>>>>>>>>>>>
+
+function toggleVideo() {
+    var videoContainer = document.getElementById('videoContainer');
+    if (videoContainer.style.display === 'none') {
+        videoContainer.style.display = 'block';
+    } else {
+        videoContainer.style.display = 'none';
+    }
+}
